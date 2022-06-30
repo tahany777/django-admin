f24ebffd68988955432b6b98286df71ada2cbba5
tahany777 <tahanymohammed777@hotmail.com>
2022-06-30 09:24:52 +0300

    feat: added custom admin sites

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class BookstoreAdminArea(admin.AdminSite):
+    site_header = 'Bookstore Admin Area'
+
+
+bookstore_site = BookstoreAdminArea(name='BookstoreAdmin')
--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog.admin import blog_site
+from bookstore.admin import bookstore_site
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogadmin/', blog_site.urls),
+    path('bookstoreadmin/', bookstore_site.urls),
 ]
 # admin.site.index_title = 'The Bookstore'
 # admin.site.site_header = 'The Bookstore Admin'
